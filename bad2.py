200bc0b09d45aefe9f9878c2c43bdfef5c93566e
Donna DeMarco <donna.demarco@seo-usa.org>
2021-06-29 17:42:46 +0000

    initial commit

---
+++ bad2.py
@@ -0,0 +1,23 @@
+    def dec(x):
+        x=x-1
+        return x
+
+    def add(x,y):
+        return x+y
+
+
+    class electro_bike:
+        def __init__(self, speed, color):
+            self.speed = speed
+            self.color = color
+
+
+        def 1_more_speed(self):
+            self.speed += 1
+
+
+    x=3
+    y=2
+
+    if x>y:
+      print(add(x,y)>dec(x))
